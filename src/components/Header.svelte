50e0822ab25956b39958b3cd40619fa9ab3b0cae
weiyinc11 <weiyinc@uchicago.edu>
2023-04-26 18:33:38 -0500

    Drop down bar

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -5,6 +5,8 @@
   import SearchBar from '$components/SearchBar.svelte';
   import { createEventDispatcher } from 'svelte';
 
+  import { slide } from 'svelte/transition'
+
   const dispatch = createEventDispatcher();
 
   $: currentRoute = $page.url.pathname;
@@ -17,9 +19,23 @@
     { name: 'donate', path: '/search/clothes' },
     { name: 'wholesale', path: '/search/wholesale'}
   ];
+
+  let sublinks = [
+    { name: 'SHOP ALL', path: '/search' },
+    { name: 'CANDLES', path: '/search/featured' },
+    { name: 'DIFFUSERS', path: '/search/clothes' },
+    { name: 'GIFT SETS', path: '/search/wholesale'},
+    { name: 'MATCHES & ACCESSORIES', path: '/search/wholesale'}
+  ]
+
   function openCart() {
     showMenu = false;
     dispatch('openCart', true);
+  }
+
+  let isExpanded = false;
+  function clickHandler(){
+    isExpanded = !isExpanded;
   }
 </script>
 
@@ -49,16 +65,31 @@
       </a>
     </div>
     <div class="hidden lg:flex items-center space-x-4">
-      {#each tabs as tab, i (tab.name)}
+      {#each tabs as tab}
+        {#if tab.name == "shop"}
+            <nav>
+                <button class="text-[#224b59] opacity-80 text-xl" on:click={clickHandler}>SHOP</button>
+                {#if isExpanded}
+                    <div class="bg-white overflow-auto absolute py-4 z-10 text-sm">
+                        <ul class="text-[#224b59] opacity-60 font-serif tracking-wider" transition:slide>
+                            <li class="bg-black opacity-50 h-[0.99px] mb-2"></li>
+                            {#each sublinks as sub}
+                            <li class="px-6 py-2"><a href="www.google.com">{sub.name}</a></li>
+                            {/each}
+                        </ul>
+                    </div>
+                {/if}
+            </nav> 
+        {:else}
         <div class:active={currentRoute === tab.path}>
-          <a
+            <a
             data-sveltekit-prefetch
             href={tab.path}
             class={`uppercase text-xl hover:text-[#991f36] px-2 py-1 text-[#224b59] transition-colors rounded-lg ${
-              currentRoute === tab.path ? 'opacity-100' : 'opacity-75'
-            }`}>{tab.name}</a
-          >
+            currentRoute === tab.path ? 'opacity-100' : 'opacity-75'
+            }`}>{tab.name}</a>
         </div>
+        {/if}
       {/each}
       <div class="ml-auto flex items-center">
         <button on:click={openCart} class="relative my-2 mx-4">
